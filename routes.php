5a9ee60dd3b34f442156445f0786300bb44bc45e
AlchemistOfWeb <nikitakzntcv@gmail.com>
2021-03-24 15:51:59 +0400

    file handling was made

--- routes.php
+++ routes.php
@@ -17,30 +17,10 @@ Route::post('/form', function(){
         && 
         $_FILES['file']['error'] == 0 ) 
     {
-        storeAsCsv( prepareForCsv($_FILES['file']), 'files/' );
+        $file_text = file_get_contents($_FILES['file']['tmp_name']);
+
+        storeAsCsv( prepareForCsv($file_text), 'files/' );
     }
 
     return redirect('/form');
 });
-
-function prepareForCsv($text)
-{
-    $pattern = "/\b[а-яА-яёЁa-zA-Z]+\b/u"; // предлоги и союзы тоже считаются словами
-
-    $num_of_words = preg_match_all($pattern, $text, $tmp);
-
-    $word_nums = [];
-
-
-    foreach ( $tmp[0] as $item ) {
-        $word_nums[$item] = isset( $word_nums[$item] ) ? $word_nums[$item] + 1 : 1;
-    }
-
-    foreach ( $word_nums as $key => $val ) {
-        $prepared_arr[] = array($key, $val);
-    }
-
-    $prepared_arr[] = array('num of words', $num_of_words);
-
-    return $prepared_arr;
-}
